0883a52fe9aeaa4e8167ee7b7f1cd7082021b02c
dadheech-vartika <63057328+dadheech-vartika@users.noreply.github.com>
2020-06-09 20:12:41 +0530

    Update ReverseString.py

--- Solutions/ReverseString.py
+++ Solutions/ReverseString.py
@@ -1,25 +1,25 @@
-Write a function that reverses a string. The input string is given as an array of characters char[].
+# Write a function that reverses a string. The input string is given as an array of characters char[].
 
-Do not allocate extra space for another array, you must do this by modifying the input array in-place with O(1) extra memory.
+# Do not allocate extra space for another array, you must do this by modifying the input array in-place with O(1) extra memory.
 
-You may assume all the characters consist of printable ascii characters.
+# You may assume all the characters consist of printable ascii characters.
 
  
 
-Example 1:
+# Example 1:
 
-Input: ["h","e","l","l","o"]
-Output: ["o","l","l","e","h"]
-Example 2:
+# Input: ["h","e","l","l","o"]
+# Output: ["o","l","l","e","h"]
+# Example 2:
 
-Input: ["H","a","n","n","a","h"]
-Output: ["h","a","n","n","a","H"]
+# Input: ["H","a","n","n","a","h"]
+# Output: ["h","a","n","n","a","H"]
 
-   Hide Hint #1  
-The entire logic for reversing a string is based on using the opposite directional two-pointer approach!
+#    Hide Hint #1  
+# The entire logic for reversing a string is based on using the opposite directional two-pointer approach!
 
 
-SOLUTION:
+# SOLUTION:
 
 
 class Solution(object):
