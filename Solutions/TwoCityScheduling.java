5a5c74f4ad0d3d4387a0b9351c67031e47660eab
dadheech-vartika <63057328+dadheech-vartika@users.noreply.github.com>
2020-06-09 20:11:12 +0530

    Update TwoCityScheduling.java

--- Solutions/TwoCityScheduling.java
+++ Solutions/TwoCityScheduling.java
@@ -1,4 +1,4 @@
-There are 2N people a company is planning to interview. The cost of flying the i-th person to city A is costs[i][0], and the cost of flying the i-th person to city B is costs[i][1].
+/*There are 2N people a company is planning to interview. The cost of flying the i-th person to city A is costs[i][0], and the cost of flying the i-th person to city B is costs[i][1].
 
 Return the minimum cost to fly every person to a city such that exactly N people arrive in each city.
 
@@ -22,6 +22,7 @@ Note:
 1 <= costs.length <= 100
 It is guaranteed that costs.length is even.
 1 <= costs[i][0], costs[i][1] <= 1000
+*/
 
 
 //SOLUTION:
